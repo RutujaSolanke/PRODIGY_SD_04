ffcac1148ecdb46debfadf882db97394d327845c
RutujaSolanke <140905463+RutujaSolanke@users.noreply.github.com>
2024-08-28 18:45:33 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,64 @@
+public class SudokuSolver {
+    private static final int SIZE = 9; // Size of the grid (9x9)
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            System.out.println("Sudoku solved successfully!");
+            printBoard(board);
+        } else {
+            System.out.println("This Sudoku puzzle cannot be solved.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) { // Find an empty cell
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num; // Tentatively place the number
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found, trigger backtracking
+                }
+            }
+        }
+        return true; // Sudoku solved
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col, int num) {
+        // Check if the number is not in the current row, column, and 3x3 subgrid
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num || 
+                board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
